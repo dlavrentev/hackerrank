e1c701f89d1a427ce145240ea483445dbb8936e6
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-16 12:54:33 +0200

    Update day1.js

--- 10-days-of-javascript/day1.js
+++ 10-days-of-javascript/day1.js
@@ -29,3 +29,30 @@ function getPerimeter(length, width) {
     perimeter = (length * 2) + (width * 2) 
     return perimeter;
 }
+
+// Day 1: Functions 
+
+/*
+ * Create the function factorial here
+ */
+function factorial(n) {
+    if (n === 0 || n === 1)
+    return 1;
+  
+  // We start the FOR loop with i = 4
+  // We decrement i after each iteration 
+  for (var i = n - 1; i >= 1; i--) {
+    // We store the value of num at each iteration
+    n = n * i; // or num *= i;
+    /* 
+                    num      var i = num - 1       num *= i         i--       i >= 1?
+    1st iteration:   5           4 = 5 - 1         20 = 5 * 4        3          yes   
+    2nd iteration:  20           3 = 4 - 1         60 = 20 * 3       2          yes
+    3rd iteration:  60           2 = 3 - 1        120 = 60 * 2       1          yes  
+    4th iteration: 120           1 = 2 - 1        120 = 120 * 1      0          no             
+    5th iteration: 120               0                120
+    End of the FOR loop 
+    */
+  }
+  return n; //120
+}
