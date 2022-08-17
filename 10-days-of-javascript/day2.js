98dae483dc7c794521c44997906fd19f0c931b5f
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-17 23:04:19 +0200

    Update day2.js

--- 10-days-of-javascript/day2.js
+++ 10-days-of-javascript/day2.js
@@ -22,3 +22,22 @@ function getGrade(score) {
 
 // Day 2: Condition Statements: Switch
 
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch (s[0]) {
+  case ("a" || "e" || "i" || "o" || "u"):
+    letter = "A";
+    break;
+  case ("b" || "c" || "d" || "f" || "g"):
+    letter = "B";
+    break;
+  case ("h" || "j" || "k" || "l" || "m"):
+    letter = "C";
+    break;
+  case ("z" || "p" || "q" || "r" || "s" || "t" || "v" || "w" || "x" || "y" || "n"):
+    letter = "D";
+    break;
+}
+    return letter;
+}
