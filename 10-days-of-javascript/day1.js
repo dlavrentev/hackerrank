d840388ec13a440502b7e97dc0e2f793635f0fdb
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-17 11:59:51 +0200

    Update day1.js

--- 10-days-of-javascript/day1.js
+++ 10-days-of-javascript/day1.js
@@ -61,3 +61,20 @@ function factorial(n) {
 // Day 1: Let and Const 
 
 
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    let r = readLine();
+    // Print the area of the circle:
+    console.log(Math.pow(r, 2) * PI);
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r)
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
