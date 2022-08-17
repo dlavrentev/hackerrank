e46d41cf09ffb422deee2fcc1192caa7b584b0af
Dimitri Lavrentev <dimitrilavrentev96@gmail.com>
2022-08-17 23:26:18 +0200

    Update day2.js

--- 10-days-of-javascript/day2.js
+++ 10-days-of-javascript/day2.js
@@ -43,3 +43,22 @@ function getLetter(s) {
 }
 
 // Day 2: Loops 
+
+function vowelsAndConsonants(s) {
+    
+    // vowels
+    for (let i = 0; i < s.length; i++) {
+        if(s.charAt(i).match(/[aeiou]/)) {
+            console.log(s.charAt(i));
+        }
+    }
+    //consonant
+    for (let i = 0; i < s.length; i++) {
+        if(s.charAt(i).match(/[^aeiou]/)) {
+            console.log(s.charAt(i));
+        }
+    }
+    
+    
+    
+}
